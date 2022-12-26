a33ca161e842dc2266e101030d6ef4e96078aa19
Vinicios Neves <vinny@macbook.lan>
2022-12-26 20:35:09 +0000

    projeto: fim da aula 1

--- src/rotas/index.tsx
+++ src/rotas/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import AreaLogada from "../paginas/AreaLogada"
+import Carrinho from "../paginas/Carrinho"
 import Categoria from "../paginas/Categoria"
 import Home from "../paginas/Home"
 import Livro from "../paginas/Livro"
@@ -11,6 +12,7 @@ const Rotas = () => {
   return (<Routes>
     <Route path='/' element={<PaginaBase />}>
       <Route path='/' element={<Home />} />
+      <Route path='/minha-sacola' element={<Carrinho />} />
       <Route path='/minha-conta' element={<AreaLogada />}>
         <Route path="pedidos" element={<Pedidos />} />
       </Route>
